e13fad2eb9f5873b2fe7c398877660490e2846a9
Herve <herve.haurice@gmail.com>
2022-01-24 09:24:33 +0100

    Added gitignore server.js and app.js

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+
+require('dotenv').config()
+
+var db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+app.use((req, res) => {
+
+    res.json({ message: 'On va manger... des chips!' });
+
+});
+
+module.exports = app;
