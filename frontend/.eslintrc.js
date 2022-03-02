4fa56b6351990e1c1bc43a50090fd71b46abff0b
Herve <herve.haurice@gmail.com>
2022-03-02 09:55:19 +0100

    Testing component

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
+    /*"eslint:recommended",
     "plugin:prettier/recommended",
+    */
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
