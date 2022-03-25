c5aaf65e56bb0206850e9bbf696031c3e1f4b2d5
Herve <herve.haurice@gmail.com>
2022-03-25 09:23:06 +0100

    added loginVue and vuelidate for form validation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/signup">Sign up!</router-link>
+    <router-link to="/signup">Sign up!</router-link> |
+    <router-link to="/login">Log in!</router-link> 
   </nav>
   <div class="container">
     <router-view></router-view>
