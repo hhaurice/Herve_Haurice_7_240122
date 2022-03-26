fe34f19e3d96770c2c9296752875f96eefeb6a22
Herve <herve.haurice@gmail.com>
2022-03-26 08:10:42 +0100

    added vuelidate and uninstalled veelidate

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,7 +1,47 @@
 <template>
   <div class="signup">
-    <h1>Sign Up now</h1>
+    <h1>Create an account</h1>
+    <p>
+    <input type="text" placeholder="name" v-model="name" />
+    </p>
+    <p>
+    <input type="text" placeholder="lastname" v-model="lastname" />
+    </p>
+    <p>
+    <input type="password" placeholder="password" v-model="password.password" />
+    </p>
+    <p>
+    <input type="password" placeholder="confirm password" v-model="password.confirm" />
+    </p>
+    <button @click="submitForm">Submit</button>
   </div>
 </template>
+
+<script>
+
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+export default {
+    data () {
+        return {
+            name: '',
+            lastname: '',
+            email: '',
+            password: {
+                password: '',
+                confirm: '',
+            }
+        }
+    },
+    methods: {
+        submitForm () {
+            alert('Form successfully submitted')
+        }
+    },
+}
+
+</script>
+
 
 
