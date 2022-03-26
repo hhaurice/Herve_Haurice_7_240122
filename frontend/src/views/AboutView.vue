ae551e105cf71d1477dcf50b10d04614a6c3f1ec
Herve <herve.haurice@gmail.com>
2022-03-26 18:43:46 +0100

    Testing Axios with API

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li v-for="user in user.data" :key="user.id">{{ user.name }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+
+  el: '#about',
+  data() {
+    return {
+      user: [],
+    }
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/users') 
+    .then(response => this.user = response)   
+    
+    }
+};
+
+</script>
+
