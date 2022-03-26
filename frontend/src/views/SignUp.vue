6342bce6d9806fd3930e0df7741e95beb34a0dc0
Herve <herve.haurice@gmail.com>
2022-03-26 10:06:36 +0100

    Implementing signUp

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -2,16 +2,24 @@
   <div class="signup">
     <h1>Create an account</h1>
     <p>
-    <input type="text" placeholder="name" v-model="name" />
+    <input type="text" placeholder="name" v-model="state.name" />
+    <span v-if="v$.name.$error">{{ v$.name.$errors[0].$message }}</span>
     </p>
     <p>
-    <input type="text" placeholder="lastname" v-model="lastname" />
+    <input type="text" placeholder="lastname" v-model="state.lastname" />
+    <span v-if="v$.lastname.$error">{{ v$.lastname.$errors[0].$message }}</span>
     </p>
     <p>
-    <input type="password" placeholder="password" v-model="password.password" />
+    <input type="email" placeholder="email" v-model="state.email" />
+    <span v-if="v$.email.$error">{{ v$.email.$errors[0].$message }}</span>
     </p>
     <p>
-    <input type="password" placeholder="confirm password" v-model="password.confirm" />
+    <input type="password" placeholder="password" v-model="state.password.password" />
+    <span v-if="v$.password.password.$error">{{ v$.password.password.$errors[0].$message }}</span>
+    </p>
+    <p>
+    <input type="password" placeholder="confirm password" v-model="state.password.confirm" />
+    <span v-if="v$.password.confirm.$error">{{ v$.password.confirm.$errors[0].$message }}</span>
     </p>
     <button @click="submitForm">Submit</button>
   </div>
@@ -20,11 +28,14 @@
 <script>
 
 import useVuelidate from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
+import { required, email, minLength, sameAs } from '@vuelidate/validators'
+
+import { reactive, computed } from 'vue' 
 
 export default {
-    data () {
-        return {
+
+setup () {
+    const state = reactive({
             name: '',
             lastname: '',
             email: '',
@@ -32,12 +43,33 @@ export default {
                 password: '',
                 confirm: '',
             }
+
+    })
+    const rules = computed(() => {
+        return {
+            name: { required },
+            lastname: { required },
+            email: { required, email },
+            password: {
+                password: { required, minLength: minLength(8) },
+                confirm: { required, sameAs: sameAs(state.password.password) },
+            },
         }
-    },
+    })
+
+    const v$ = useVuelidate(rules, state)
+
+    return { state, v$ }
+  },
     methods: {
         submitForm () {
-            alert('Form successfully submitted')
-        }
+            this.v$.$validate()
+            if (!this.v$.$error) {
+                alert('Form successfully submitted')
+            } else {
+                alert('Form failed validation')
+            }
+        },
     },
 }
 
