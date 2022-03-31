f8fc126317a89e74da57127a81c167744fd6a46e
Herve <herve.haurice@gmail.com>
2022-03-31 15:14:24 +0200

    testing counter

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    msg: "Je rêve, çar marche pour de vrai???"
+    msg: "Je rêve, ça marche pour de vrai???",
+    counter: 0
   },
   getters: {
   },
   mutations: {
+    decreaseCounter(state) {
+      state.counter--
+    },
+    increaseCounter(state) {
+      state.counter++
+    }
   },
   actions: {
   },
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-    <p class="msg">{{ $store.state.msg }} </p>
+    <p class="msg">{{ $store.state.msg }}</p>
+    <div class="counter">{{ $store.state.counter }}</div>
+    <div class="buttons">
+      <button @click="$store.commit('decreaseCounter')">-</button>
+      <button @click="$store.commit('increaseCounter')">+</button>
+    </div>
 </template>
 
 <script>
@@ -11,5 +16,17 @@ export default {
 
 </script>
 
+<style>
+div {
+  margin-bottom: 10px;
+}
 
+.counter {
+  font-size: 80px;
+}
+
+.buttons button {
+  padding: 8px 32px 8px 32px;
+}
+</style>
 
